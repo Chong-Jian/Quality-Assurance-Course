12c2b025b433dd602525ab10be720d5bd2ed07b7
Chong Jian Ho <hochongjian@gmail.com>
2024-05-06 17:50:23 +0000

    gitpod /workspace/boilerplate-mochachai (main) $ git remote set-url origin https://github.com/Chong-Jian/Quality-Assurance-Course.git gitpod /workspace/boilerplate-mochachai (main) $ git branch -M main gitpod /workspace/boilerplate-mochachai (main) $ git push -u origin main

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -33,10 +33,10 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.isNotNull(12, '12', 'Numbers are coerced into strings with ==');
-      assert.isNotNull({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.isNotNull(6 * '2', '12');
-      assert.isNotNull(6 + '2', '12');
+      assert.equal(12, '12', 'Numbers are coerced into strings with ==');
+      assert.equal({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.notEqual(6 * '2', '12');
+      assert.notEqual(6 + '2', '12');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
